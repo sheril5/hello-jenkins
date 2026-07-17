9722c18003e7e190ab235ef21b3608ae4466eaed
sheril5 <fiosheril5.com>
2026-07-17 11:24:10 +0530

    Initial commit

---
+++ src/test/java/com/example/hello_jenkins/HelloJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
